cccc92d3b2d59b66db28b62c2f0f3f43f6199ddb
medhavi <medhavi.srivastava16@gmail.com>
2020-06-29 21:29:04 +0530

    Added FCM

--- app/src/main/java/com/news/newsapp/MainActivity.java
+++ app/src/main/java/com/news/newsapp/MainActivity.java
@@ -1,9 +1,15 @@
 package com.news.newsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.news.newsapp.api.ApiClient;
 
 import retrofit2.Call;
@@ -32,5 +38,17 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        /*FirebaseMessaging.getInstance().subscribeToTopic("NewsApp")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "Hello";
+                        if (!task.isSuccessful()) {
+                            msg = "Failed";
+                        }
+                        Log.d("TAG", msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });*/
     }
 }
---
+++ app/src/main/java/com/news/newsapp/MyMessagingService.java
@@ -0,0 +1,35 @@
+package com.news.newsapp;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.RingtoneManager;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        showNotification(remoteMessage.getNotification().getTitle() , remoteMessage.getNotification().getBody());
+    }
+    public void showNotification(String title , String msg)
+    {
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id")
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setStyle(new NotificationCompat.BigTextStyle())
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
+
